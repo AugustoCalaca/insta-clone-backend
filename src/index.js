b3815a69a50f49330e2efc15922bd2d6d47b3412
Augusto Calaca <augusto@Calaca.Augusto>
2019-06-17 20:53:02 -0300

    solve error on socket client side

--- src/index.js
+++ src/index.js
@@ -21,6 +21,6 @@ app.use(cors());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 app.use('/', require('./routes'));
 
-app.listen(3000, _ => {
+server.listen(3000, _ => {
   console.log('server running port 3000');
 });
